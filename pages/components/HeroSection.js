2e48de3d00bec86fb37ba7281852fe4a65e7b898
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-04-25 23:07:12 +0545

    commit

--- pages/components/HeroSection.js
+++ pages/components/HeroSection.js
@@ -2,97 +2,9 @@ import React, { useState, useRef, useEffect } from "react";
 import Image from "next/image";
 import styles from "./../../styles/HeroSection.module.css";
 import Navbar from "./Navbar";
+import imageArray from "./BooksData";
 
 const HeroSection = () => {
-  let imageArray = [
-    {
-      name: "The Subtle Art of Not Giving a Fuck",
-      description: "Hello guys this is a descriptionsdsdf",
-      author: "Anish Raj Pandsey",
-    },
-    {
-      name: "How to Win Friends and Influence People",
-      description: "Hello guys this is a descriptionuykjh",
-      author: "Anish Raj Pxcxzandey",
-    },
-    {
-      name: "Outliers The Story of Success",
-      description: "Hello guys this is a descriptionghjygh",
-      author: "Anish Raj Psdfsfandey",
-    },
-    {
-      name: "The 7 Habits of Highly Effective People",
-      description: "Hello guys this is a descriptionsawdssef",
-      author: "Anish Raj Padsfsdfendey",
-    },
-    {
-      name: "Who Moved My Cheese",
-      description: "Hello guys this is a description sa",
-      author: "Anish Raj Pandserfey",
-    },
-    {
-      name: "The Power of Habit Why We Do What We Do in Life and Business",
-      description: "Hello guys this is a descriptiondhyjhmnv",
-      author: "Anish Raj Perwrweandey",
-    },
-    {
-      name: "The Secret",
-      description: "Hello guys this is a descriptionfrty",
-      author: "Anish Raj werwerPandey",
-    },
-    {
-      name: "Thinking, Fast and Slow",
-      description: "Hello guys this is a descriptionxzfsaw",
-      author: "Anish Raj Panweresdfdey",
-    },
-    {
-      name: "Quiet The Power of Introverts in a World That Can't Stop Talking",
-      description: "Hello guys this is a descriptionsdasda",
-      author: "Anish Raj Paertretndey",
-    },
-    {
-      name: "Think and Grow Rich",
-      description: "Hello guys this is a descriptionasdas",
-      author: "Anish Raj Panreyfhfdey",
-    },
-    {
-      name: "The Power of Now",
-      description: "Hello guys this is a descriptionsdhfbvc",
-      author: "Anish Raj Panddgdfgdrgey",
-    },
-    {
-      name: "Atomic Habits",
-      description: "Hello guys this is a descriptionzfdsdgf ",
-      author: "Anish Raj Pandert54yey",
-    },
-
-    {
-      name: "Harry Potter",
-      description: "Hello guys this is a descriptiond",
-      author: "Anish Raj Panetetrdey",
-    },
-    {
-      name: "A Brief History of Time",
-      description: "Hello guys this is a descriptionasfs",
-      author: "Anish Raj Panregddey",
-    },
-    {
-      name: "Our Little World",
-      description: "Hello guys this is a descriptionsertgf",
-      author: "Anish Raj Panreterydey",
-    },
-    {
-      name: "The Lords of the Rings",
-      description: "Hello guys this is a descriptionsaewtr",
-      author: "Anish Raj Panadawedey",
-    },
-    {
-      name: "The Little Prince",
-      description: "Hello guys this is a descriptionjyuhj",
-      author: "Anish Raj Panwqwetrgddey",
-    },
-  ];
-
   const [RandomIndex, setRandomIndex] = useState(
     Math.ceil(Math.random() * -1 * imageArray.length)
   );
@@ -100,8 +12,8 @@ const HeroSection = () => {
   useEffect(() => {
     const StartInternal = setInterval(() => {
       //todo set random letter
-      // setRandomIndex(Math.ceil(Math.random() * -1 * imageArray.length));
-      setRandomIndex(RandomIndex > -17 ? RandomIndex - 1 : RandomIndex + 1);
+      setRandomIndex(Math.ceil(Math.random() * -1 * imageArray.length));
+      // setRandomIndex(RandomIndex > -17 ? RandomIndex - 1 : RandomIndex + 1);
       // setRandomIndex();
     }, 40000);
     return () => {
@@ -126,10 +38,11 @@ const HeroSection = () => {
           priority={false}
         ></Image>
         <div className={styles.contentBox}>
-          <h1>Bibliophile</h1>
+          <h1>A community of Bibliophile</h1>
           <section className={styles.productShowcase}>
             <div className={styles.left}>
               <h2 className={styles.BookListHeading}>
+                <span>Trending Books: </span>
                 {imageArray[-1 * RandomIndex].name}
               </h2>
               {/* imageArray[-1 * RandomIndex] gives the book which is selected */}
