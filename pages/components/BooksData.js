347295ba6cd92cc015ac831d352c814e982bdd7d
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-04-27 23:03:04 +0545

    ad

---
+++ pages/components/BooksData.js
@@ -0,0 +1,105 @@
+const imageArray = [
+  {
+    name: "The Subtle Art of Not Giving a Fuck",
+    description:
+      "'s not about being indifferent, but being comfortable with being different. To not give a fuck about adversity, you must first care about something more important than adversity. You are always choosing what to give a fuck about.",
+    author: "Here's Manson",
+  },
+  {
+    name: "How to Win Friends and Influence People",
+    description:
+      "The core idea is that you can change other people's behavior simply by changing your own. It teaches you the principles to better understand people, become a more likable person, improve relationships, win others over, and influence behavior through leadership.",
+    author: "Dale Carnegie",
+  },
+  {
+    name: "Outliers The Story of Success",
+    description:
+      "Outliers explains why “the self-made man” is a myth and what truly lies behind the success of the best people in their field, which is often a series of lucky events, rare opportunities and other external factors, which are out of our control.",
+    author: "Malcolm Gladwell",
+  },
+  {
+    name: "The 7 Habits of Highly Effective People",
+    description:
+      "The 7 Habits of Highly Effective People by Stephen R. Covey is a self-improvement book. It is written on Covey's belief that the way we see the world is entirely based on our own perceptions. In order to change a given situation, we must change ourselves, and in order to change ourselves, we must be able to change our perceptions.",
+    author: "Stephen Covey",
+  },
+  {
+    name: "Who Moved My Cheese",
+    description:
+      "Who moved my cheese revolves around the four characters who live in a maze. All of them love cheese. When the cheese disappears, Sniff and Scurry head into the maze to search for new cheese while Hem and Haw feel betrayed and start complaining that someone had stolen their cheese from them",
+    author: "Spencer Johnson",
+  },
+  {
+    name: "The Power of Habit Why We Do What We Do in Life and Business",
+    description:
+      "The Power Of Habit helps you understand why habits are at the core of everything you do, how you can change them, and what impact that will have on your life, your business and society. Subscribe to listen to an unlimited number of full articles.",
+    author: "Charles Duhigg",
+  },
+  {
+    name: "The Secret",
+    description:
+      " The Secret is a self-help book by Rhonda Byrne that explains how the law of attraction, which states that positive energy attracts positive things into your life, governs your thinking and actions, and how you can use the power of positive thinking to achieve anything you can imagine.",
+    author: "Rhonda Byrne",
+  },
+  {
+    name: "Thinking, Fast and Slow",
+    description:
+      "Thinking Fast And Slow shows you how two systems in your brain are constantly fighting over control of your behavior and actions, and teaches you the many ways in which this leads to errors in memory, judgment and decisions, and what you can do about it.",
+    author: "Daniel Kahneman",
+  },
+  {
+    name: "Quiet The Power of Introverts in a World That Can't Stop Talking",
+    description:
+      "Introverts tend to enjoy quiet concentration, listen more than they talk, and think before they speak, and have a more circumspect and cautious approach to risk. Introverts think more, are less reckless and focus on what really matters—relationships and meaningful work.",
+    author: "Susan Cain",
+  },
+  {
+    name: "Think and Grow Rich",
+    description:
+      "Think And Grow Rich is a curation of the 13 most common habits of wealthy and successful people, distilled from studying over 500 individuals over the course of 20 years. If The Intelligent Investor is the bible for investing, then Think And Grow Rich is the bible for successful people in general.",
+    author: "Napolean Hill",
+  },
+  {
+    name: "The Power of Now",
+    description:
+      "The Power of Now shows you that every minute you spend worrying about the future or regretting the past is a minute lost, because really all you have to live in is the present, the now, and gives you actionable strategies to start living every minute as it occurs",
+    author: "Eckhart Tolle",
+  },
+  {
+    name: "Atomic Habits",
+    description:
+      "Atomic Habits will teach you how to make small changes that will transform your habits and deliver amazing results.Atomic Habits is the definitive guide to breaking bad behaviors and adopting good ones in four steps, showing you how small, incremental, everyday routines compound into massive, positive change over time.",
+    author: "James Clear",
+  },
+
+  {
+    name: "Harry Potter",
+    description:
+      "Adaptation of the first of J.K. Rowling's popular children's novels about Harry Potter, a boy who learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own. He is summoned from his life as an unwanted child to become a student at Hogwarts, an English boarding school for wizards. There, he meets several friends who become his closest allies and help him discover the truth about his parents' mysterious deaths.",
+    author: "JK Rolling",
+  },
+  {
+    name: "A Brief History of Time",
+    description:
+      " A Brief History Of Time is Stephen Hawking's way of explaining the most complex concepts and ideas of physics, such as space, time, black holes, planets, stars and gravity to the average Joe, so that even you and I can better understand how our planet was created, where it came from, and where it's ",
+    author: "Stephen Hawking",
+  },
+  {
+    name: "Our Little World",
+    description:
+      "Perfect for readers of Celeste Ng's Everything I Never Told You, Our Little World is a powerful and lyrical coming-of-age story that examines the complicated bond of sisterhood, the corrosive power of envy, and how the traumas of our youths can shape our identities for a lifetime.",
+    author: "Karen Winn",
+  },
+  {
+    name: "The Lords of the Rings",
+    description: "Hello guys this is a descriptionsaewtr",
+    author: "Anish Raj Panadawedey",
+  },
+  {
+    name: "The Little Prince",
+    description:
+      "A pilot, stranded in the desert, meets a little boy who is a prince on a planet. Based on the story by Antoine de Saint-Exupery, this magical musical fable begins as a pilot makes a forced landing on the barren Sahara Desert. He is befriended by a 'little' prince from the planet Asteroid B-612.",
+    author: "Antoine de Saint-Exupéry",
+  },
+];
+export default imageArray;
---
+++ public/fontawesome.js
@@ -0,0 +1,13 @@
+// import the library
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+// import your icons
+import { faMoneyBill } from "@fortawesome/pro-solid-svg-icons";
+import { faCode, faHighlighter } from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faMoneyBill,
+  faCode,
+  faHighlighter
+  // more icons go here
+);
