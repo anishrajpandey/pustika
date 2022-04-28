988185142c11e3908da7b57457ba4fec3009b65c
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-04-28 19:31:44 +0545

    page title

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import HeroSection from "./components/HeroSection";
-import Script from "next/script";
+
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Script
-        src="https://kit.fontawesome.com/3d2f093b4a.js"
-        crossorigin="anonymous"
-        strategy="lazyOnload"
-      ></Script>
+      <Head>
+        <title>Pustika | A community of Bibliophiles.</title>
+      </Head>
 
       <HeroSection />
     </>
