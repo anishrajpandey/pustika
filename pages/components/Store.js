c415898a91dfa07614c8de3a81d60486cfc46dd9
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-05-03 23:16:32 +0545

    adding pages

---
+++ pages/components/Store.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Store = () => {
+  return (
+    <>
+      {/* <Navbar /> */}
+      This is a store
+    </>
+  );
+};
+
+export default Store;
