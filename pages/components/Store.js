c42c584c83b002affd674733f8450460d4268fb6
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-05-05 00:31:06 +0545

    changes

--- pages/components/Store.js
+++ pages/components/Store.js
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import imageArray from "../../public/BooksData";
 
 const Store = () => {
+  var [datajson, setdatajson] = useState();
   useEffect(async () => {
     let data = await fetch("http://localhost:3000/api/books");
-    let datajson = await data.json();
-
-    console.log(datajson);
+    let { imageArray } = await data.json();
+    setdatajson(imageArray);
   }, []);
+  console.log(typeof datajson);
+  // for (let element of {datajson}) {
+  //   console.log(element);
+  // }
   return (
     <>
       <div className="store"></div>
