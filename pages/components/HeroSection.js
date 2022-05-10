7b6a87a7061b771c56f6d2e155a864c4fab37cc6
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-05-10 15:21:41 +0545

    commit

--- pages/components/HeroSection.js
+++ pages/components/HeroSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "./../../styles/HeroSection.module.css";
 import Link from "next/link";
