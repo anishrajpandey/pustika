20df305af161d94582b612e6b89f70298e05d1e7
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-05-11 21:03:06 +0545

    sell books page creation

---
+++ pages/components/SellBooks.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SellBooks = () => {
+  return (
+    <div>
+      <h1>Enter book info</h1>
+      <label htmlFor="name">Name Of Book</label>
+      <input type="text" id="name" />
+      <label htmlFor="description">Description Of Book</label>
+      <input type="text" id="description" />
+      <label htmlFor="description">Name Of Book</label>
+      <input type="text" id="name" />
+      <label htmlFor="name">Name Of Book</label>
+      <input type="text" id="name" />
+    </div>
+  );
+};
+
+export default SellBooks;
