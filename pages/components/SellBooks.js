3749f6f9edbe91088fdca6c9ee5e9c5815f76354
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-05-11 21:43:19 +0545

    changes

--- pages/components/SellBooks.js
+++ pages/components/SellBooks.js
@@ -3,23 +3,29 @@ import styles from "./../../styles/SellBooks.module.css";
 const SellBooks = () => {
   return (
     <div>
-      <h1>Enter book info</h1>
-      <label className={styles.label} htmlFor="name">
-        Name Of Book
-      </label>
-      <input type="text" id="name" />
-      <label className={styles.label} htmlFor="description">
-        Description Of Book
-      </label>
-      <input type="text" id="description" />
-      <label className={styles.label} htmlFor="image">
-        Image
-      </label>
-      <input type="file" id="image" />
-      <label className={styles.label} htmlFor="price">
-        Price
-      </label>
-      <input type="number" id="price" />
+      <h1 className={styles.heading}>Add your Book to Store</h1>
+      <form className={styles.form} action="/">
+        <label className={styles.label} htmlFor="name">
+          Name Of Book
+        </label>
+        <input type="text" id="name" />
+        <label className={styles.label} htmlFor="description">
+          Description Of Book
+        </label>
+        <textarea rows="8" cols="40" id="description" />
+        <label className={styles.label} htmlFor="image">
+          Image<i> (portrait)</i>
+        </label>
+        <input type="file" id="image" />
+        <label className={styles.label} htmlFor="price">
+          Price<i>(in numbers)</i>
+        </label>
+        <input type="number" className={`${styles.price}`} id="price" />
+
+        <button type={"submit"} className={`${styles.sellbtn} btn-primary`}>
+          Add to selling list
+        </button>
+      </form>
     </div>
   );
 };
