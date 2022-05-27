a56053ce1a245c546e95327b0e0d3ff1add56f3d
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-05-27 21:20:11 +0545

    updates

--- pages/api/addProducts.js
+++ pages/api/addProducts.js
@@ -4,9 +4,5 @@ const handler = async (req, res) => {
   let products = await product.find();
   res.status(200).json({ products });
 };
-const connectToDatabase = () => {
-  mongoose.connect(
-    "mongodb+srv://anishrajpandey:anishrajpandey@cluster0.xaa3c.mongodb.net/test"
-  );
-};
+
 export default handler;
