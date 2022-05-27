33a82af09bd9ea891cda9d218e5e55397e7d3c2d
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-05-27 21:06:15 +0545

    changes_update

--- pages/api/addProducts.js
+++ pages/api/addProducts.js
@@ -1,8 +1,12 @@
-import connectDb from "../../middleware/mongoose";
+import mongoose from "mongoose";
 import product from "../../models/product";
 const handler = async (req, res) => {
   let products = await product.find();
   res.status(200).json({ products });
 };
-
+const connectToDatabase = () => {
+  mongoose.connect(
+    "mongodb+srv://anishrajpandey:anishrajpandey@cluster0.xaa3c.mongodb.net/test"
+  );
+};
 export default handler;
