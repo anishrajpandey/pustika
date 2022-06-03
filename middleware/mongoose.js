506e86231f933dcf4ad600bd9cf86f0c5c2ecd60
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-06-03 18:33:23 +0545

    front end fixes

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
-const connectDb = async (handler) => {
-  if (mongoose.connections[0].readyState) {
-    return handler(req, res);
-  }
-  await mongoose.connect(process.env.MONGODB_URI);
-  return handler(req, res);
-};
-export default connectDb;
+// import mongoose from "mongoose";
+// const connectDb = async (handler) => {
+//   if (mongoose.connections[0].readyState) {
+//     return handler(req, res);
+//   }
+//   await mongoose.connect(process.env.MONGODB_URI);
+//   return handler(req, res);
+// };
+// export default connectDb;
--- pages/api/addProducts.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import product from "../../models/product";
-const handler = async (req, res) => {
-  let products = await product.find();
-  res.status(200).json({ products });
-};
-
-export default handler;
