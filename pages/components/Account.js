159930db4e9a9e9854c3aef24acc179a34f21afc
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-06-03 20:02:41 +0545

    login page

--- pages/components/Account.js
+++ pages/components/Account.js
@@ -1,11 +1,41 @@
 import styles from "./../../styles/Account.module.css";
-
+import { useState } from "react";
 const Account = () => {
+  const [TranslateProperties, setTranslateProperties] = useState({
+    coverOffset: "0",
+  });
+  const handleClick = (e) => {
+    console.log(e.target.id);
+    e.target.id === "login"
+      ? setTranslateProperties({ coverOffset: "50%" })
+      : setTranslateProperties({ coverOffset: "0%" });
+  };
   return (
-    <div className={styles.main}>
-      <div className={styles.login}></div>
-      <div className={styles.signup}></div>
-    </div>
+    <main className={styles.main}>
+      <style jsx>{`
+        .cover {
+          left: ${TranslateProperties.coverOffset};
+        }
+      `}</style>
+      <div className={styles.switch}>
+        <span className={styles.label}></span>
+        <span onClick={handleClick} id="login">
+          LOGIN
+        </span>
+        <span onClick={handleClick} id="signup">
+          SIGN UP
+        </span>
+      </div>
+      <div className={`${styles.cover} cover`}></div>
+      <div className={styles.mainBox}>
+        <div className={styles.login}>
+          <h1>LOGIN HERE</h1>
+        </div>
+        <div className={styles.signup}>
+          <h1>SIGN UP HERE</h1>
+        </div>
+      </div>
+    </main>
   );
 };
 
