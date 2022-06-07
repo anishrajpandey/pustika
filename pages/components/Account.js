1158916f07f14b71f96dd368be65eb42a176cc20
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-06-07 22:08:23 +0545

    account pages fixes

--- pages/components/Account.js
+++ pages/components/Account.js
@@ -1,15 +1,38 @@
 import styles from "./../../styles/Account.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import FontAwesomeIcon, { faArrowLeft } from "./assets/FontAwesome";
 const Account = () => {
+  const coverRef = useRef();
   const [TranslateProperties, setTranslateProperties] = useState({
     coverOffset: "0",
   });
+  const [DescriptionProperties, setDescriptionProperties] = useState({
+    title: "Login To Your Account",
+    description: "Login with your Enail Id and Password to buy or sell books",
+    rotate: "180deg",
+  });
   const handleClick = (e) => {
     console.log(e.target.id);
-    e.target.id === "signup"
-      ? setTranslateProperties({ coverOffset: "50%" })
-      : setTranslateProperties({ coverOffset: "0%" });
+
+    if (e.target.id === "signup") {
+      setTranslateProperties({ coverOffset: "50%" });
+      setDescriptionProperties({
+        title: "Create A New Account",
+        description: "Its absolutely free and It just takes couple of seconds.",
+        rotate: "0deg",
+      });
+      coverRef.current.style.setProperty("--rotateDegree", "0deg");
+      console.log(coverRef.current);
+    } else {
+      setTranslateProperties({ coverOffset: "0%" });
+      setDescriptionProperties({
+        title: "Login To Your Account",
+        description:
+          "Login with your Enail Id and Password to buy or sell books",
+        rotate: "180deg",
+      });
+      coverRef.current.style.setProperty("--rotateDegree", "180deg");
+    }
   };
   return (
     <main className={styles.main}>
@@ -32,10 +55,17 @@ const Account = () => {
         </span>
       </div>
       <div className={`${styles.cover} cover`}>
-        <div className={styles.signupInfo}>
-          <h2>Create an Account</h2>
-          <p>Its free and It just takes couple of seconds. </p>
-          <FontAwesomeIcon icon={faArrowLeft} className={styles.arrow} />
+        <div className={styles.signUpInfo} ref={coverRef}>
+          <h2>{DescriptionProperties.title}</h2>
+          <p>{DescriptionProperties.description} </p>
+          <FontAwesomeIcon
+            icon={faArrowLeft}
+            className={styles.arrow}
+            style={{
+              transform: `rotate(${DescriptionProperties.rotate})`,
+            }}
+          />
+          {console.log(DescriptionProperties.rotate)}
         </div>
         {/* <div className={styles.loginInfo}></div> */}
       </div>
