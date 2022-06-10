4959bf1c439609e57fe241e042f22a238633e6dc
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-06-10 21:20:35 +0545

    fixing on deployment

--- pages/components/Account.js
+++ pages/components/Account.js
@@ -39,86 +39,106 @@ const Account = () => {
     }
   };
   return (
-    <main className={styles.main}>
-      <style jsx>{`
-        .cover {
-          left: ${TranslateProperties.coverOffset};
-        }
+    <div className={styles.maincontainer}>
+      <div className={styles.signupOrLogin}>
+        <main className={styles.main}>
+          <style jsx>{`
+            .cover {
+              left: ${TranslateProperties.coverOffset};
+            }
 
-        .label {
-          left: ${TranslateProperties.coverOffset};
-        }
-      `}</style>
-      <div className={styles.switch}>
-        <span className={`${styles.label} label`}></span>
-        <span onClick={handleClick} id="login" className={styles.loginText}>
-          LOGIN
-        </span>
-        <span onClick={handleClick} id="signup" className={styles.signupText}>
-          SIGN UP
-        </span>
-      </div>
-      <div className={`${styles.cover} cover`}>
-        <div className={styles.signUpInfo} ref={coverRef}>
-          <h2>{DescriptionProperties.title}</h2>
-          <p>{DescriptionProperties.description} </p>
-          <FontAwesomeIcon
-            icon={faArrowLeft}
-            className={styles.arrow}
-            style={{
-              transform: `rotate(${DescriptionProperties.rotate})`,
-            }}
-          />
-          {console.log(DescriptionProperties.rotate)}
-        </div>
-      </div>
-      <div className={styles.mainBox}>
-        <div className={styles.signup}>
-          <h3>SIGN UP HERE</h3>
-          <label htmlFor="name">
-            Name
-            <input id="name" type="text" placeholder="John Doe"></input>
-          </label>
-          <label htmlFor="email">
-            Email
-            <input id="email" type="text" placeholder="example@xyz.com"></input>
-          </label>
-          <label htmlFor="password">
-            Password
-            <input id="password" type="password" placeholder="*****"></input>
-          </label>
-          <label htmlFor="confirmPassword">
-            Confirm Password
-            <input
-              id="confirmPassword"
-              type="password"
-              placeholder="******"
-            ></input>
-          </label>
-          <div className={styles.buttonContainer}>
-            <button className={`btn-primary ${styles.signUpButton}`}>
-              Create Account
-            </button>
+            .label {
+              left: ${TranslateProperties.coverOffset};
+            }
+          `}</style>
+          <div className={styles.switch}>
+            <span className={`${styles.label} label`}></span>
+            <span onClick={handleClick} id="login" className={styles.loginText}>
+              LOGIN
+            </span>
+            <span
+              onClick={handleClick}
+              id="signup"
+              className={styles.signupText}
+            >
+              SIGN UP
+            </span>
           </div>
-        </div>
-        <div className={styles.login}>
-          <h3>LOGIN HERE</h3>
-          <label htmlFor="loginName">Enter Your Email Id</label>
-          <input type="email" placeholder="your email" id="loginName"></input>
-          <label htmlFor="passwordLogin">Enter Your Password</label>
-          <input
-            type="password"
-            placeholder="your password"
-            id="passwordLogin"
-          ></input>
-          <div className={styles.buttonContainer}>
-            <button className={`btn-primary ${styles.signUpButton}`}>
-              Create Account
-            </button>
+          <div className={`${styles.cover} cover`}>
+            <div className={styles.signUpInfo} ref={coverRef}>
+              <h2>{DescriptionProperties.title}</h2>
+              <p>{DescriptionProperties.description} </p>
+              <FontAwesomeIcon
+                icon={faArrowLeft}
+                className={styles.arrow}
+                style={{
+                  transform: `rotate(${DescriptionProperties.rotate})`,
+                }}
+              />
+              {console.log(DescriptionProperties.rotate)}
+            </div>
           </div>
-        </div>
+          <div className={styles.mainBox}>
+            <div className={styles.signup}>
+              <h3>SIGN UP HERE</h3>
+              <label htmlFor="name">
+                Name
+                <input id="name" type="text" placeholder="John Doe"></input>
+              </label>
+              <label htmlFor="email">
+                Email
+                <input
+                  id="email"
+                  type="text"
+                  placeholder="example@xyz.com"
+                ></input>
+              </label>
+              <label htmlFor="password">
+                Password
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="*****"
+                ></input>
+              </label>
+              <label htmlFor="confirmPassword">
+                Confirm Password
+                <input
+                  id="confirmPassword"
+                  type="password"
+                  placeholder="******"
+                ></input>
+              </label>
+              <div className={styles.buttonContainer}>
+                <button className={`btn-primary ${styles.signUpButton}`}>
+                  Create Account
+                </button>
+              </div>
+            </div>
+            <div className={styles.login}>
+              <h3>LOGIN HERE</h3>
+              <label htmlFor="loginName">Enter Your Email Id</label>
+              <input
+                type="email"
+                placeholder="your email"
+                id="loginName"
+              ></input>
+              <label htmlFor="passwordLogin">Enter Your Password</label>
+              <input
+                type="password"
+                placeholder="your password"
+                id="passwordLogin"
+              ></input>
+              <div className={styles.buttonContainer}>
+                <button className={`btn-primary ${styles.signUpButton}`}>
+                  Login
+                </button>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
-    </main>
+    </div>
   );
 };
 export default Account;
