5bd5f3fda220f7f052799d20a329cf01c189f7c6
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-06-12 22:16:15 +0545

    cleanups

--- pages/api/addBook.js
+++ pages/api/addBook.js
@@ -1,17 +1,6 @@
 import finalModel from "../../models/BookModel";
 import connectToDB from "../../connectMongo";
-// export default async function handler(req, res) {
-//   try {
-//     await connectToDB();
-//     const request = await finalModel.create(req.body);
-//     res.json({ request });
-//     console.log("Pass");
-//   } catch (e) {
-//     console.error(e);
-//     res.json({ e });
-//     console.log("Fail");
-//   }
-// }
+
 export default async function addTest(req, res) {
   try {
     console.log("CONNECTING TO MONGO");
