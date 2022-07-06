d05fd49b285a9d83644194c373325db315394283
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-07-06 23:28:40 +0545

    updates

--- pages/components/Cart.js
+++ pages/components/Cart.js
@@ -104,4 +104,4 @@ const Cart = () => {
 };
 
 export default Cart;
-// hello world this is  a simple message and i want to say that i am alright
+// hello world this is  a simple message and i want to say that i am
