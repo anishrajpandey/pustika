687aeb01bab6f2fdd9d8f0a9c987b84992fb70e9
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-07-27 22:49:13 +0545

    otp success

--- pages/components/Account.js
+++ pages/components/Account.js
@@ -167,7 +167,7 @@ const Account = ({ pageurl }) => {
         }
         
       })
-      // good touching on your b
+      // good touchi
       e.target.disabled = false;
     },2000)
       console.log(typeof ChangedUserData?.phone)
