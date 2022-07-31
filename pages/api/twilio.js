75ae27b8662fb07b2faeead7fd90a67756d5a47d
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-07-31 21:57:58 +0545

    otp success

--- pages/api/twilio.js
+++ pages/api/twilio.js
@@ -2,7 +2,7 @@ import twilio from "twilio";
 export default async function handler(req, res) {
   if (req.method==='POST') {
     const { from, to, message } = JSON.parse(req.body);
-    // console.log(to, from, message);
+
     console.log(   process.env.TWILIO_SID, 
       process.env.TWILIO_AUTH)
 
