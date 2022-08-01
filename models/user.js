a9e6892fbab49761bcbb75858b38bccd714b0a52
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-08-01 22:44:23 +0545

    dp

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    userImage: { type: String },
   },
   {
     timestamps: true,
--- pages/api/updateUser.js
+++ pages/api/updateUser.js
@@ -1,14 +1,23 @@
 import connectToDB from "../../connectMongo";
-import User from "../../models/user";
+import user from "../../models/user";
 export default async function hanlder(req, res) {
   if (req.method === "POST") {
     await connectToDB();
     // if (userImage in JSON.parse(req.body)) {
+    delete JSON.parse(req.body)._id;
+    delete JSON.parse(req.body).createdAt;
+    delete JSON.parse(req.body).updatedAt;
+    delete JSON.parse(req.body).__v;
     console.log(JSON.parse(req.body));
-    let returnedData = await User.updateOne(
-      { username: JSON.parse(req.body).username + "" },
+    let returnedData = await user.updateOne(
+      { username: JSON.parse(req.body).username },
       { $set: { userImage: JSON.parse(req.body).userImage } }
     );
+    let document = await user.findOne(
+      { username: JSON.parse(req.body).username }
+      // { $set: { userImage: JSON.parse(req.body).userImage } }
+    );
+    console.log(document);
     res.status(200).json({ message: returnedData });
   }
 }
