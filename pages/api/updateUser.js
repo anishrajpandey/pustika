41cfb69a5a182b29e8c64fe7be9c40744756d4d8
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-08-01 22:23:28 +0545

    profile picture update

---
+++ pages/api/updateUser.js
@@ -0,0 +1,21 @@
+import connectToDB from "../../connectMongo";
+import User from "../../models/user";
+export default async function hanlder(req, res) {
+  if (req.method === "POST") {
+    await connectToDB();
+    // if (userImage in JSON.parse(req.body)) {
+    console.log(JSON.parse(req.body));
+    let returnedData = await User.updateOne(
+      { username: JSON.parse(req.body).username + "" },
+      { $set: { userImage: JSON.parse(req.body).userImage } }
+    );
+    res.status(200).json({ message: returnedData });
+
+    // }
+    //
+
+    //         awaitq.body.username}, {$set: {phone: JSON.parse(req.body).phoneNumber}});
+
+    // }
+  }
+}
