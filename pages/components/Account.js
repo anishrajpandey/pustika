7a9d471b1050e74035af0dc39ccf94b7055ee25f
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-08-02 21:54:54 +0545

    update

--- pages/components/Account.js
+++ pages/components/Account.js
@@ -206,7 +206,7 @@ const Account = ({ pageurl }) => {
       e.target.disabled = false;
     }, 2000);
   };
-  // jg kg
+  // jg
   return (
     <div className={styles.maincontainer}>
       {IsAuthorized && (
