bf48f09657a6379058f564b85b919f8ef2d69685
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-08-03 20:48:30 +0545

    update

--- pages/components/Account.js
+++ pages/components/Account.js
@@ -206,7 +206,7 @@ const Account = ({ pageurl }) => {
       e.target.disabled = false;
     }, 2000);
   };
-  // hellow world is a common naS
+  // hellow world is a S
   return (
     <div className={styles.maincontainer}>
       {IsAuthorized && (
