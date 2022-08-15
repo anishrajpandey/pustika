bcb168307b428bfd51dda13653091b1f3d689d50
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-08-15 21:57:05 +0545

    update

--- pages/api/twilio.js
+++ pages/api/twilio.js
@@ -1,24 +1,20 @@
 import twilio from "twilio";
 export default async function handler(req, res) {
-  if (req.method==='POST') {
+  if (req.method === "POST") {
     const { from, to, message } = JSON.parse(req.body);
 
-    console.log(   process.env.TWILIO_SID, 
-      process.env.TWILIO_AUTH)
+    console.log(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
 
-    let twilloSDK = twilio(
-      process.env.TWILIO_SID, 
-      process.env.TWILIO_AUTH
-    );
-    let tres = await twilloSDK.messages
-      .create({
-        from,
-        to,
-        body: message,
-      });
+    let twilloSDK = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
+    let tres = await twilloSDK.messages.create({
+      from,
+      to,
+      body: message,
+    });
     res.status(200).json(tres);
-    console.log(tres)
+    console.log(tres);
   } else {
     res.json({ err: "ERROR" });
   }
 }
+// fuck mee
