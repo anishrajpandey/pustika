718dabf80ecceb0b2b7fb61b2402e565118c58eb
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-08-21 22:40:22 +0545

    update

--- pages/api/twilio.js
+++ pages/api/twilio.js
@@ -17,4 +17,4 @@ export default async function handler(req, res) {
     res.json({ err: "ERROR" });
   }
 }
-// hi my name is anish raj pandey and i am a good boy aree yo?
+// hi my name is any aree yo?
