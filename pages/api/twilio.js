5cda0061326cc6c52c6b805f402fe40af515dc35
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-08-25 21:37:07 +0545

    updates

--- pages/api/twilio.js
+++ pages/api/twilio.js
@@ -17,4 +17,4 @@ export default async function handler(req, res) {
     res.json({ err: "ERROR" });
   }
 }
-// HEILLO HYYA THIS A IS  DOESNOT MAKE NAY SENSE
+// HEILLO HYYA THIS A IS   MAKE NAY SENSE
