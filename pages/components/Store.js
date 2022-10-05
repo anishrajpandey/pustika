dfb761f7e88ef88eb054bb44377a140e0ea01822
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-10-05 16:17:40 +0545

    updates

--- pages/components/Store.js
+++ pages/components/Store.js
@@ -25,7 +25,7 @@ const Store = ({ url }) => {
     console.log(data);
     localStorage.setItem("cart", JSON.stringify({ CartItems }));
   };
-// hello world
+  // hello world
   return (
     <>
       <Head>
@@ -72,3 +72,4 @@ export async function getServerSideProps() {
   };
 }
 export default Store;
+// dfsd
