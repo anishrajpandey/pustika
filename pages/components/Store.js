b3791b77c22e005401f884a947c610bfec5382d9
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-10-06 19:47:38 +0545

    update

--- pages/components/Store.js
+++ pages/components/Store.js
@@ -25,7 +25,7 @@ const Store = ({ url }) => {
     console.log(data);
     localStorage.setItem("cart", JSON.stringify({ CartItems }));
   };
-  // hello world
+  // hello wor
   return (
     <>
       <Head>
