03b40734c3eff24fd7347ad2c486043a29b3e594
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-10-11 19:32:47 +0545

    notification ui

---
+++ pages/utils/Notification.js
@@ -0,0 +1,20 @@
+import styles from "./../../styles/Notification.module.css";
+
+const Notification = () => {
+  return (
+    <div className={styles.mainNotification}>
+      <ul>
+        <h3 align="center">Your Notifications</h3>
+        <li>Notification-01</li>
+        <li>Notification-02</li>
+        <li>Notification-03</li>
+        <li>Notification-04</li>
+        <li>Notification-05</li>
+        <li>Notification-06</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Notification;
+//  utu t
